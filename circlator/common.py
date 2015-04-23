dea8d411400a6f7768239fe8fe7ba58a80aafb08
Martin Hunt <mh12@sanger.ac.uk>
2015-04-23 14:03:44 +0100

    do not print errors if allowed to fail

--- circlator/common.py
+++ circlator/common.py
@@ -10,14 +10,13 @@ def syscall(cmd, allow_fail=False, verbose=False):
         subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as error:
         errors = error.output.decode()
-        print('The following command failed with exit code', error.returncode, file=sys.stderr)
-        print(cmd, file=sys.stderr)
-        print('\nThe output was:\n', file=sys.stderr)
-        print(errors, file=sys.stderr, flush=True)
-
         if allow_fail:
             return False, errors
         else:
+            print('The following command failed with exit code', error.returncode, file=sys.stderr)
+            print(cmd, file=sys.stderr)
+            print('\nThe output was:\n', file=sys.stderr)
+            print(errors, file=sys.stderr, flush=True)
             sys.exit(1)
 
     return True, None
