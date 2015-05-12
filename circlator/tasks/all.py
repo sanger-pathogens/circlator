48a72bb5708da143c951840fb8183ebbf6311f25
Martin Hunt <mh12@sanger.ac.uk>
2015-05-12 08:49:22 +0100

    Change default merge values

--- circlator/tasks/all.py
+++ circlator/tasks/all.py
@@ -32,8 +32,8 @@ def run():
     #assemble_group = parser.add_argument_group('assemble_group')
 
     merge_group = parser.add_argument_group('merge options')
-    merge_group.add_argument('--merge_min_id', type=float, help='Nucmer minimum percent identity [%(default)s]', metavar='FLOAT', default=99)
-    merge_group.add_argument('--merge_min_length', type=int, help='Minimum length of hit for nucmer to report [%(default)s]', metavar='INT', default=4000)
+    merge_group.add_argument('--merge_min_id', type=float, help='Nucmer minimum percent identity [%(default)s]', metavar='FLOAT', default=98)
+    merge_group.add_argument('--merge_min_length', type=int, help='Minimum length of hit for nucmer to report [%(default)s]', metavar='INT', default=500)
     parser.add_argument('--merge_min_length_merge', type=int, help='Minimum length of nucmer hit to use when merging [%(default)s]', metavar='INT', default=4000)
     merge_group.add_argument('--merge_breaklen', type=int, help='breaklen option used by nucmer [%(default)s]', metavar='INT', default=500)
     merge_group.add_argument('--merge_ref_end', type=int, help='max distance allowed between nucmer hit and end of input assembly contig [%(default)s]', metavar='INT', default=15000)
