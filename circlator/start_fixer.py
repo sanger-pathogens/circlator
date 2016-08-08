a6aef1aa6bb23b1e73144d810bf2885f54262255
Martin Hunt <mh12@sanger.ac.uk>
2016-08-08 14:01:29 +0100

    New methods _find_circular_using_promer _find_circular_using_prodigal

--- circlator/start_fixer.py
+++ circlator/start_fixer.py
@@ -1,6 +1,8 @@
 import os
 import shutil
+import copy
 import tempfile
+from distutils.version import LooseVersion
 import pyfastaq
 import pymummer
 import circlator
@@ -39,6 +41,8 @@ class StartFixer:
             with open(ignore) as f:
                 self.ignore = {x.rstrip().split()[0] for x in f}
 
+        self.contigs_fa_with_ends = self.outprefix + '.contigs_plus_ends.fa'
+
 
     @classmethod
     def _max_length_from_fasta_file(cls, infile):
@@ -71,3 +75,104 @@ class StartFixer:
 
         pyfastaq.utils.close(f)
 
+
+    @classmethod
+    def _find_circular_using_promer(cls, outprefix, ref_genes_fa, contigs_dict, min_percent_id, end_length, log_fh, ignore=None):
+        if ignore is None:
+            ignore = set()
+        promer_out = outprefix + '.promer'
+        contigs_with_ends = outprefix + '.contigs_with_ends.fa'
+        StartFixer._write_fasta_plus_circularized_ends(contigs_dict, contigs_with_ends, end_length, ignore=ignore)
+
+        prunner = pymummer.nucmer.Runner(
+            contigs_with_ends,
+            ref_genes_fa,
+            promer_out,
+            min_id=min_percent_id,
+            promer=True,
+            verbose=True,
+        )
+        prunner.run()
+
+        circularized = {} # original_contig_name -> promer match
+        file_reader = pymummer.coords_file.reader(promer_out)
+
+        for match in file_reader:
+            if match.ref_name.endswith('__ends'):
+                original_contig_name = match.ref_name[:-len('__ends')]
+            else:
+                original_contig_name = match.ref_name
+
+            if match.hit_length_qry == match.qry_length and match.percent_identity >= min_percent_id:
+                if original_contig_name not in circularized or circularized[original_contig_name].percent_identity < match.percent_identity:
+                    circularized[original_contig_name] = copy.copy(match)
+
+        if len(circularized):
+            print('Using the following promer matches to circularize contigs:', file=log_fh)
+            for contig_name in circularized:
+                print(circularized[contig_name], file=log_fh)
+        else:
+            print('No suitable promer matches found', file=log_fh)
+
+        return circularized
+
+
+    @classmethod
+    def _find_circular_using_prodigal(cls, outprefix, contigs_dict, circular_from_promer, log_fh):
+        if len(contigs_dict) == len(circular_from_promer):
+            return {}
+
+        prodigal = circlator.external_progs.make_and_check_prog('prodigal')
+        total_contig_length = sum([len(contigs_dict[x]) for x in contigs_dict if x not in circular_from_promer])
+        prodigal_input_file = outprefix + '.for_prodigal.fa'
+        prodigal_output_file = outprefix + '.prodigal.gff'
+        with open(prodigal_input_file, 'w') as f:
+            for seq_name in sorted(contigs_dict):
+                if seq_name not in circular_from_promer:
+                    print(contigs_dict[seq_name], file=f)
+
+        if (total_contig_length < 20000):
+            # prodigal needs -p meta option for sequences less than 20000
+            # annoyingly newer version of prodigal has different -p option!
+            if LooseVersion(prodigal.version()) >= LooseVersion('3.0'):
+                p_option = "-p anon"
+            else:
+                p_option = "-p meta"
+
+            cmd = ' '.join([
+              'prodigal',
+              '-i', prodigal_input_file,
+              '-o', prodigal_output_file,
+              '-f gff -c -q',
+              p_option
+            ])
+
+        circlator.common.syscall(cmd)
+        circularized = {}
+        best_dist = {}
+
+        with open(prodigal_output_file) as f:
+            for line in f:
+                if line.startswith('#'):
+                    continue
+                contig, x, y, start, end, *z = line.rstrip().split('\t')
+                new_inter = pyfastaq.intervals.Interval(int(start), int(end))
+                new_dist = new_inter.distance_to_point(int(len(contigs_dict[contig])))
+
+                if contig in circularized:
+                    assert contig in best_dist
+                    if new_dist < best_dist[contig]:
+                        best_dist[contig] = new_dist
+                        circularized[contig] = line.rstrip()
+                else:
+                    best_dist[contig] = new_dist
+                    circularized[contig] = line.rstrip()
+
+        if len(circularized):
+            print('Using the following prodigal predictions to circularize contigs:', file=log_fh)
+            for contig_name in circularized:
+                print(circularized[contig_name], file=log_fh)
+        else:
+            print('No prodigal matches found', file=log_fh)
+
+        return circularized
--- circlator/tests/start_fixer_test.py
+++ circlator/tests/start_fixer_test.py
@@ -3,7 +3,9 @@ import shutil
 import filecmp
 import copy
 import os
+import sys
 import pyfastaq
+import pymummer
 from circlator import start_fixer
 
 modules_dir = os.path.dirname(os.path.abspath(start_fixer.__file__))
@@ -54,3 +56,41 @@ class TestStartFixer(unittest.TestCase):
         start_fixer.StartFixer._write_fasta_plus_circularized_ends(contigs, tmp_out, 5, ignore={'seq1', 'seq4'})
         self.assertTrue(filecmp.cmp(expected_ignore, tmp_out, shallow=False))
         os.unlink(tmp_out)
+
+
+    def test_find_circular_using_promer(self):
+        '''Test _find_circular_using_promer'''
+        contigs_infile = os.path.join(data_dir, 'start_fixer_find_circular_using_promer.contigs.fa')
+        ref_genes_fa = os.path.join(data_dir, 'start_fixer_find_circular_using_promer.refs.fa')
+        tmp_outprefix = 'tmp.start_fixer_find_circular_using_promer'
+        contigs_dict = {}
+        pyfastaq.tasks.file_to_dict(contigs_infile, contigs_dict)
+        end_extend = start_fixer.StartFixer._max_length_from_fasta_file(ref_genes_fa)
+
+        expected = {
+            'ctg_DNAA_ECOLI': pymummer.alignment.Alignment('1021\t2424\t1\t1404\t1404\t1404\t100.00\t.\t.\t3404\t1404\t1\t1\tctg_DNAA_ECOLI\tsp|P03004|DNAA_ECOLI\n'),
+            'ctg_DNAA1_CHLPN': pymummer.alignment.Alignment('685\t2067\t1\t1383\t1383\t1383\t100.00\t.\t.\t2808\t1383\t1\t1\tctg_DNAA1_CHLPN__ends\tsp|Q9Z8M9|DNAA1_CHLPN\n'),
+            'ctg_DNAA_ECOLI_2': pymummer.alignment.Alignment('901\t2304\t1\t1404\t1404\t1404\t100.00\t.\t.\t3044\t1404\t1\t1\tctg_DNAA_ECOLI_2\tsp|P03004|DNAA_ECOLI_2\n'),
+}
+
+        got = start_fixer.StartFixer._find_circular_using_promer(tmp_outprefix, ref_genes_fa, contigs_dict, 70, end_extend, sys.stdout)
+        self.assertEqual(expected, got)
+        os.unlink(tmp_outprefix + '.contigs_with_ends.fa')
+        os.unlink(tmp_outprefix + '.promer')
+
+
+    def test_find_circular_using_prodigal(self):
+        '''test _find_circular_using_prodigal'''
+        contigs_infile = os.path.join(data_dir, 'start_fixer_find_circular_using_prodigal.ctgs.fa')
+        outprefix = 'tmp.test_find_circular_using_prodigal'
+        contigs_dict = {}
+        pyfastaq.tasks.file_to_dict(contigs_infile, contigs_dict)
+        circular_from_promer = {'ctg3'}
+        got = start_fixer.StartFixer._find_circular_using_prodigal(outprefix, contigs_dict, circular_from_promer, sys.stdout)
+        expected = {
+            'ctg1': 'ctg1\tProdigal_v2.60\tCDS\t2429\t2689\t3.3\t-\t0\tID=1_2;partial=00;start_type=ATG;rbs_motif=TAA;rbs_spacer=15bp;gc_cont=0.517;conf=67.99;score=3.28;cscore=-0.50;sscore=3.78;rscore=3.23;uscore=-3.07;tscore=4.12;'
+        }
+        self.assertEqual(expected, got)
+        os.unlink(outprefix + '.for_prodigal.fa')
+        os.unlink(outprefix + '.prodigal.gff')
+
